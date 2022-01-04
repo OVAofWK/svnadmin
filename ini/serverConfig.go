8d81718827ed4655ccaf4640cbc528da5f4c2c3b
wujiayu <wujiayu@example.com>
2022-01-04 14:09:33 +0800

    增加日志落盘功能

--- ini/serverConfig.go
+++ ini/serverConfig.go
@@ -3,6 +3,7 @@ package ini
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
@@ -15,8 +16,10 @@ type Config struct {
 	} `yaml:"server"`
 
 	Admin struct {
-		User   string `yaml:"user"`
-		Passwd string `yaml:"passwd"`
+		User    string `yaml:"user"`
+		Passwd  string `yaml:"passwd"`
+		LogPath string `yaml:"logPath"`
+		UseLog  bool   `yaml:"useLog"`
 	} `yaml:"admin"`
 	Web struct {
 		Title string `yaml:"title"`
@@ -35,4 +38,18 @@ func ReadConfYaml(confPath string) Config {
 		panic(err)
 	}
 	return config
+}
+func isAxist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		if os.IsNotExist(err) {
+			return false
+		}
+		fmt.Println(err)
+		return false
+	}
+	return true
 }
--- svnadmin.go
+++ svnadmin.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"regexp"
 	"svn/ini"
 	"svn/web"
+	"time"
 
 	assetfs "github.com/elazarl/go-bindata-assetfs"
 	"github.com/gin-gonic/gin"
 )
 
+// 读svnadmin配置文件
 var CONFIG = ini.ReadConfYaml("conf/svnconf.yaml")
 var BasicUser = map[string]string{CONFIG.Admin.User: CONFIG.Admin.Passwd}
 
@@ -18,6 +22,35 @@ type H map[string]string
 
 func NewWeb() *gin.Engine {
 	r := gin.Default()
+
+	// 日志写盘
+	if CONFIG.Admin.UseLog {
+		if isExist(CONFIG.Admin.LogPath) {
+			currentTime := time.Now().Format("2006-01-02-150405")
+			oldLog := CONFIG.Admin.LogPath + "-" + currentTime
+			os.Rename(CONFIG.Admin.LogPath, oldLog)
+		}
+		f, _ := os.Create(CONFIG.Admin.LogPath)
+		gin.DefaultWriter = io.MultiWriter(f)
+
+		r.Use(gin.Recovery())
+		// gin.DisableConsoleColor()
+		r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+			return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+			)
+		}))
+	}
+
+	// 加载静态资源
 	fsCss := assetfs.AssetFS{Asset: web.Asset, AssetDir: web.AssetDir, AssetInfo: web.AssetInfo, Prefix: "web/css"}
 	fsJs := assetfs.AssetFS{Asset: web.Asset, AssetDir: web.AssetDir, AssetInfo: web.AssetInfo, Prefix: "web/js"}
 	fs := assetfs.AssetFS{Asset: web.Asset, AssetDir: web.AssetDir, AssetInfo: web.AssetInfo, Prefix: "web"}
@@ -27,11 +60,26 @@ func NewWeb() *gin.Engine {
 	return r
 }
 
+func isExist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		if os.IsNotExist(err) {
+			return false
+		}
+		fmt.Println(err)
+		return false
+	}
+	return true
+}
+
+// 响应get请求
 func Getconf(c *gin.Context) {
-	svnParam := c.Param("svn_param")
+	svnParam := c.Param("param_1")
 	info := ini.ReadConf(CONFIG, svnParam)
 	path := "web/" + svnParam + ".html"
-
 	c.Writer.WriteHeader(200)
 	adminHtml, _ := web.Asset(path)
 	// 将html中的{{}}内的信息替换为变量内容
@@ -39,13 +87,13 @@ func Getconf(c *gin.Context) {
 	c.Writer.Write(html)
 }
 
+// 响应post请求
 func Postconf(c *gin.Context) {
-	svnParam := c.Param("svn_param")
+	svnParam := c.Param("param_1")
 	info := c.PostForm("info")
 	err := ini.WriteConf(CONFIG, svnParam, info)
 	if err == nil {
 		path := "/admin/" + svnParam
-		// c.HTML(http.StatusOK, path, gin.H{"title": CONFIG.Web.Title, "info": info})
 		//重定向至提交的当前页
 		c.Redirect(http.StatusMovedPermanently, path)
 	} else {
@@ -86,14 +134,18 @@ func Route(r *gin.Engine) *gin.Engine {
 	})
 	admin := r.Group("/admin", gin.BasicAuth(BasicUser))
 	{
-		admin.GET("/:svn_param/", Getconf)
-		admin.POST("/:svn_param/", Postconf)
+		admin.GET("/:param_1/", Getconf)
+		admin.POST("/:param_1/", Postconf)
 	}
 	return r
 }
 
 func main() {
+	if !isExist("conf/svnconf.yaml") {
+		fmt.Println("未找到svnadmin配置文件 conf/svnconf.yaml")
+	}
 	r := NewWeb()
 	r = Route(r)
 	r.Run(CONFIG.Server.Listen)
+
 }
